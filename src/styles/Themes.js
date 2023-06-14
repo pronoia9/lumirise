31e8d118565cf3f26d0d09af11222e0c16dc29ae
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-14 17:31:53 +0300

    add background 2

--- src/styles/Themes.js
+++ src/styles/Themes.js
@@ -1,17 +1,18 @@
 const darkTheme = {
   background: '#222831',
+  background2: '#31363c',
   font: '#eaeaea',
   accent: '#29a587',
 };
 
 const lightTheme = {
   background: '#eaeaea',
+  background2: '',
   font: '#222831',
   accent: '#29a587',
 };
 
 export { darkTheme, lightTheme };
-
 
 `
 #ffffff
