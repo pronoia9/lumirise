6ba5598e1b3197739b045eb08f04e176263f1173
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-14 18:30:27 +0300

    add side menu data

---
+++ src/utils/data.js
@@ -0,0 +1,16 @@
+export const sideMenuData = {
+  links: [
+    { title: 'Home', link: '/' },
+    { title: '', link: '' },
+    { title: '', link: '' },
+    { title: '', link: '' },
+    { title: '', link: '' },
+    { title: '', link: '' },
+    { title: '', link: '' },
+  ],
+  social: [
+    { title: 'GitHub', link: '', icon: '' },
+    { title: 'Facebook', link: '', icon: '' },
+    { title: '', link: '', icon: '' },
+  ],
+};
