4b76b9ce599ff17daac9ca9e9104b1d721ed6139
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-14 20:52:49 +0300

    add sidebar button motion

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -1,8 +1,16 @@
 import { easeIn, easeInOut } from 'framer-motion';
 
-export const sidebar = ({ height }) => ({
-  open: (height = 1000) => ({
-    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+// Hamburger <-> Close
+export const siderbarButtonMotion = [
+  { variants: { closed: { d: 'M 2 2.5 L 20 2.5' }, open: { d: 'M 3 16.5 L 17 2.5' } } },
+  { variants: { closed: { opacity: 1 }, open: { opacity: 0 } }, transition: { duration: 0.1 } },
+  { variants: { closed: { d: 'M 2 16.346 L 20 16.346' }, open: { d: 'M 3 2.5 L 17 16.346' } } },
+];
+
+// Circle Background Open <-> Close
+export const sidebarBackgroundMotion = {
+  open: () => ({
+    clipPath: `circle(${window.innerHeight * 2}px at 8% 6%)`,
     transition: {
       type: 'spring',
       stiffness: 20,
@@ -10,7 +18,7 @@ export const sidebar = ({ height }) => ({
     },
   }),
   closed: {
-    clipPath: 'circle(30px at 40px 40px)',
+    clipPath: 'circle(0px at 8% 6%)',
     transition: {
       delay: 0.5,
       type: 'spring',
@@ -18,7 +26,7 @@ export const sidebar = ({ height }) => ({
       damping: 40,
     },
   },
-});
+};
 
 export const sideMenuMotion = (menuOpen) => {
   const transition = { type: 'tween', delay: 0, duration: 1, ease: easeInOut };
