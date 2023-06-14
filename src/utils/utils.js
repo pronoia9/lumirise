c0fb28a41e7bdab2c5d470dff6184384dcf65e02
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-14 17:20:57 +0300

    add toggle theme func

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,6 +4,8 @@ export const getSystemTheme = () => (window?.matchMedia('(prefers-color-scheme: 
 
 export const getTheme = (theme) => (theme === 'light' ? lightTheme : darkTheme);
 
+export const toggleTheme = (setTheme) => setTheme((prev) => prev === 'dark' ? 'light' : 'dark');
+
 export const isDarkTheme = (theme) => theme === 'dark';
 
 export const systemThemeChangeHandler = (e, setTheme) => { setTheme(e.matches ? 'dark' : 'light'); };
