36c7336c6012d230e68c8612d0e3c3aa84b3f2a4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-14 19:32:18 +0300

    remove use dimensions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,29 +1,11 @@
-import { useEffect, useRef } from 'react';
 import { darkTheme, lightTheme } from '../styles/Themes';
 
 export const getSystemTheme = () => (window?.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 
 export const getTheme = (theme) => (theme === 'light' ? lightTheme : darkTheme);
 
-export const toggleTheme = (setTheme) => setTheme((prev) => (prev === 'dark' ? 'light' : 'dark'));
+export const toggleTheme = (setTheme) => setTheme((prev) => prev === 'dark' ? 'light' : 'dark');
 
 export const isDarkTheme = (theme) => theme === 'dark';
 
-export const systemThemeChangeHandler = (e, setTheme) => {
-  setTheme(e.matches ? 'dark' : 'light');
-};
-
-// Naive implementation - in reality would want to attach
-// a window or resize listener. Also use state/layoutEffect instead of ref/effect
-// if this is important to know on initial client render.
-// It would be safer to  return null for unmeasured states.
-export const useDimensions = (ref) => {
-  const dimensions = useRef({ width: 0, height: 0 });
-
-  useEffect(() => {
-    dimensions.current.width = ref.current.offsetWidth;
-    dimensions.current.height = ref.current.offsetHeight;
-  }, []);
-
-  return dimensions.current;
-};
+export const systemThemeChangeHandler = (e, setTheme) => { setTheme(e.matches ? 'dark' : 'light'); };
