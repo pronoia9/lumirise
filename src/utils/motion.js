ab282f81abdcac8e4c4bef1876e3f2197189ee10
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-15 00:54:18 +0300

    format

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -5,6 +5,7 @@ export const sidebarMotion = {
     { variants: { closed: { opacity: 1 }, open: { opacity: 0 } }, transition: { duration: 0.1 } },
     { variants: { closed: { d: 'M 2 16.346 L 20 16.346' }, open: { d: 'M 3 2.5 L 17 16.346' } } },
   ],
+
   // Circle Background Open <-> Close
   background: {
     open: () => ({
@@ -25,6 +26,8 @@ export const sidebarMotion = {
       },
     },
   },
+
+  // Sidebar Links List
   links: {
     open: {
       transition: { staggerChildren: 0.07, delayChildren: 0.2 },
@@ -33,6 +36,8 @@ export const sidebarMotion = {
       transition: { staggerChildren: 0.05, staggerDirection: -1 },
     },
   },
+
+  // Sidebar Link Item
   linkItem: {
     open: {
       y: 0,
@@ -49,6 +54,8 @@ export const sidebarMotion = {
       },
     },
   },
+
+  // Sidebar Socials List
   socials: {
     open: {
       transition: { staggerChildren: 0.07, delayChildren: 0.33 },
@@ -57,6 +64,8 @@ export const sidebarMotion = {
       transition: { staggerChildren: 0.05, staggerDirection: -1 },
     },
   },
+
+  // Sidebar Social Item
   socialItem: {
     open: {
       scale: 1,
@@ -71,84 +80,6 @@ export const sidebarMotion = {
       transition: {
         scale: { stiffness: 1000 },
       },
-    },
-  },
-};
-
-export const siderbarButtonMotion = [
-  { variants: { closed: { d: 'M 2 2.5 L 20 2.5' }, open: { d: 'M 3 16.5 L 17 2.5' } } },
-  { variants: { closed: { opacity: 1 }, open: { opacity: 0 } }, transition: { duration: 0.1 } },
-  { variants: { closed: { d: 'M 2 16.346 L 20 16.346' }, open: { d: 'M 3 2.5 L 17 16.346' } } },
-];
-
-export const sidebarBackgroundMotion = {
-  open: () => ({
-    clipPath: `circle(${window.innerHeight * 2}px at 8% 6%)`,
-    transition: {
-      type: 'spring',
-      stiffness: 20,
-      restDelta: 2,
-    },
-  }),
-  closed: {
-    clipPath: 'circle(0px at 8% 6%)',
-    transition: {
-      delay: 0.5,
-      type: 'spring',
-      stiffness: 400,
-      damping: 40,
-    },
-  },
-};
-
-export const sidebarLinksMotion = {
-  open: {
-    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
-  },
-  closed: {
-    transition: { staggerChildren: 0.05, staggerDirection: -1 },
-  },
-};
-
-export const sidebarLinkItemMotion = {
-  open: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      y: { stiffness: 1000, velocity: -100 },
-    },
-  },
-  closed: {
-    y: 50,
-    opacity: 0,
-    transition: {
-      y: { stiffness: 1000 },
-    },
-  },
-};
-
-export const sidebarSocialsMotion = {
-  open: {
-    transition: { staggerChildren: 0.07, delayChildren: 0.33 },
-  },
-  closed: {
-    transition: { staggerChildren: 0.05, staggerDirection: -1 },
-  },
-};
-
-export const sidebarSocialItemMotion = {
-  open: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      scale: { stiffness: 1000, velocity: -100 },
-    },
-  },
-  closed: {
-    scale: 0,
-    opacity: 0,
-    transition: {
-      scale: { stiffness: 1000 },
     },
   },
 };
