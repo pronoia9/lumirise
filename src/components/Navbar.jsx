7ddb13c788da3a46dee31314d48444c92267e565
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-14 23:07:20 +0300

    add init line and pattern

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,20 @@ import { motion, useCycle } from 'framer-motion';
 
 import { logo } from '../assets';
 import { MoonSVG, SunSVG, SidebarButton } from './SVGs';
-import { sideMenuData } from '../utils/data';
-import { sidebarBackgroundMotion } from '../utils/motion';
+import { sidebarData } from '../utils/data';
+import { sidebarBackgroundMotion, sidebarMenuItemMotion, sidebarMenuMotion } from '../utils/motion';
 import { isDarkTheme, toggleTheme } from '../utils/utils';
 
 export default function Navbar({ theme, setTheme }) {
   const [isOpen, toggleOpen] = useCycle(false, true);
   const containerRef = useRef(null);
 
-  const handleThemeClick = () => { toggleTheme(setTheme); };
-  const handleMenuClick = () => { toggleOpen(); };
+  const handleThemeClick = () => {
+    toggleTheme(setTheme);
+  };
+  const handleMenuClick = () => {
+    toggleOpen();
+  };
 
   return (
     <>
@@ -37,19 +41,41 @@ export default function Navbar({ theme, setTheme }) {
         </Icons>
       </Container>
 
-      {/* TEST */}
-      <SidebarWrapper ref={containerRef} initial={false} animate={isOpen ? 'open' : 'closed'} custom={containerRef?.current?.offsetHeight}>
-        <SidebarBackground variants={sidebarBackgroundMotion} />
-        <SidebarMenu>
-          <li>asdasd</li>
-          <li>asdasd</li>
-          <li>asdasd</li>
-          <li>asdasd</li>
-        </SidebarMenu>
-      </SidebarWrapper>
+      {/* Sidebar Menu */}
+      <SidebarMenu initial={false} animate={isOpen ? 'open' : 'closed'}>
+        <SidebarContainer>
+          {/* Line */}
+          <SidebarLine>
+            <SidebarDots />
+          </SidebarLine>
 
-      {/* Menu */}
-      {/* <Menu key={`menu-${isOpen}`} {...sideMenuMotion(isOpen)} open={isOpen}></Menu> */}
+          <SidebarWrapper>
+            {/* Links */}
+            <Links>
+              {sidebarData.links.map((link, index) => (
+                <>
+                  <Link to='/'>{link.title}</Link>
+                </>
+              ))}
+            </Links>
+            {/* Socials */}
+            <Socials>
+              {sidebarData.socials.map((social, index) => (
+                <>O</>
+              ))}
+            </Socials>
+          </SidebarWrapper>
+        </SidebarContainer>
+        <SidebarBackground variants={sidebarBackgroundMotion} />
+        {/* <PREVSidebarMenu variants={sidebarMenuMotion}>
+          {[0, 1, 2, 3, 4].map((i) => (
+            <motion.p variants={sidebarMenuItemMotion} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
+              <div style={{ border: `2px solid ${['#FF008C', '#D309E1', '#9C1AFF', '#7700FF', '#4400FF'][i]}` }} />
+              <div style={{ border: `2px solid ${['#FF008C', '#D309E1', '#9C1AFF', '#7700FF', '#4400FF'][i]}` }} />
+            </motion.p>
+          ))}
+        </PREVSidebarMenu> */}
+      </SidebarMenu>
     </>
   );
 }
@@ -114,35 +140,100 @@ const Icons = styled.div`
   }
 `;
 
-const MenuPREV = styled(motion.div)`
-  position: fixed;
-  /* right: -612px; */
-  top: 0;
-  right: 0;
-  width: 512px;
-  height: 100%;
-  background: ${({ theme }) => theme.background2};
-  z-index: 2;
-  /* transition: all 1s cubic-bezier(0.3, 0, 0.3, 1); */
-`;
-
-const SidebarWrapper = styled(motion.nav)`
+const SidebarMenu = styled(motion.nav)`
   position: fixed;
   top: 0;
   right: 0;
   width: 100%;
   max-width: 512px;
   height: 100%;
+  display: flex;
+  overflow: hidden;
+`;
+
+const SidebarContainer = styled(motion.div)`
+  /* position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  -webkit-overflow-scrolling: touch;
+  overflow: auto; */
+  position: relative;
+  width: 100%;
+  margin: auto;
+  padding: 0 1.5rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  z-index: 1;
+
+  background-color: green;
+`;
+
+const SidebarWrapper = styled(motion.div)``;
+
+const Links = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Socials = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const SidebarLine = styled.div`
+  position: relative;
+  left: 7.5%;
+  width: 2px;
+  background: ${({ theme }) => theme.lineBorder};
+  pointer-events: none;
+  isolation: isolate;
+  z-index: 2;
+
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    left: -1rem;
+    width: 1.75rem;
+    height: 1.75rem;
+    border-radius: 50%;
+    background: ${({ theme }) => theme.lineBackground};
+    border: 2px solid ${({ theme }) => theme.lineBorder};
+    box-shadow: ${({ theme }) => theme.lineShadow};
+  }
+  &:before {
+    top: -1rem;
+  }
+  &:after {
+    bottom: -1rem;
+  }
+`;
+
+const SidebarDots = styled.div`
+  position: absolute;
+  top: -185px;
+  left: -170px;
+  width: 226px;
+  height: 226px;
+  background-image: url('images/pat-1.png');
+  background-repeat: no-repeat;
+  background-size: contain;
+  filter: invert(${({ theme }) => theme.imageInvert});
+  opacity: 0.6;
+  z-index: -1;
 `;
 
 const SidebarBackground = styled(motion.div)`
+  position: absolute;
+  top: 0;
+  right: 0;
   background: ${({ theme }) => theme.background2};
   width: 100%;
   height: 100%;
   transform: scaleX(-1);
-`;
-
-const SidebarMenu = styled(motion.ul)`
-  width: 100%;
-  height: 100%;
 `;
