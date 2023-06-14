534ab5ccf600da263717800f102690b9b14c59c9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-14 18:30:49 +0300

    add animate presence

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx';
+import { AnimatePresence } from 'framer-motion';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <AnimatePresence>
+      <App />
+    </AnimatePresence>
   </BrowserRouter>
 );
