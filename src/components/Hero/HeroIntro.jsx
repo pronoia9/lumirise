c60574d280821e62138503637bbea2a3b2bc8a93
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-15 23:54:26 +0300

    remove unused styled components

--- src/components/Hero/HeroIntro.jsx
+++ src/components/Hero/HeroIntro.jsx
@@ -11,9 +11,9 @@ export default function HeroTitle() {
     <Container className='content scrolla-element-anim-1 scroll-animate animate__active animate__animated'>
       {/* Title */}
       <Titles className='titles'>
-        <SubtiteTop><p>Hello, <span>my name is</span></p></SubtiteTop>
-        <TitleContainer><span>{name.split(' ')[0]}</span> {name.split(' ')[1]}</TitleContainer>
-        <SubtiteBottom>I am a <span>{occupation}</span></SubtiteBottom>
+        <SubtitleWithAccent><p>Hello, <span>my name is</span></p></SubtitleWithAccent>
+        <Title><span>{name.split(' ')[0]}</span> {name.split(' ')[1]}</Title>
+        <SubtitleWithHandwriting>I am a <span>{occupation}</span></SubtitleWithHandwriting>
       </Titles>
 
       {/* Description */}
@@ -60,12 +60,6 @@ const Titles = styled.div`
     min-height: 160px;
   }
 `;
-
-const SubtiteTop = styled(SubtitleWithAccent)``;
-
-const SubtiteBottom = styled(SubtitleWithHandwriting)``;
-
-const TitleContainer = styled(Title)``;
 
 const DescriptionContainer = styled(Description)`
   padding: 40px 0 40px 0;
