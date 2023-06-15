10ed5cbdddac6484438ea62e5847e0cc5ced7ccb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-15 20:45:19 +0300

    add gradients numbers to components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,11 +3,11 @@ import { Hero, Skills, Works, Experience, Contact } from '../components';
 export default function HomePage() {
   return (
     <>
-      <Hero />
-      <Skills />
-      <Works />
-      <Experience />
-      <Contact />
+      <Hero gradient={1} />
+      <Skills gradient={2} />
+      <Works gradient={1} />
+      <Experience gradient={2} />
+      <Contact gradient={1} />
     </>
   );
 }
