787d2cbb0c23ce9e68bde60359b61343bbdfb766
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-15 21:27:36 +0300

    add subtitle text styles

---
+++ src/styles/TextStyles.js
@@ -0,0 +1,22 @@
+import { styled } from 'styled-components';
+import { motion } from 'framer-motion';
+
+export const Subtitle = styled(motion.div)`
+  font-size: 1rem;
+  font-weight: 700;
+  color: var(--c-font);
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
+`;
+
+export const SubtitleWithAccent = styled(Subtitle)`
+  span {
+    color: #29a587;
+  }
+`;
+
+export const SubtitleWithHandwriting = styled(Subtitle)`
+  span {
+    font-family: var(--f-secondary);
+  }
+`;
