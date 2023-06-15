d9eb893bc5269ac82772628a7f57e56bdcdb2d5b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-15 22:32:57 +0300

    add some styles

--- src/components/Hero/HeroIntro.jsx
+++ src/components/Hero/HeroIntro.jsx
@@ -1,39 +1,65 @@
 import { styled } from 'styled-components';
 import { heroData } from '../../utils/data';
 import Socials from '../Socials';
-import { SubtitleWithAccent, SubtitleWithHandwriting, Title } from '../../styles/TextStyles';
+import { Description, SubtitleWithAccent, SubtitleWithHandwriting, Title } from '../../styles/TextStyles';
 
 export default function HeroTitle() {
   const { name, occupation, intro } = heroData;
 
   return (
-    <div>
+    <Container className='CONTAINER'>
       {/* Title */}
-      <div>
-        {/* Subtitle Top */}
-        <SubtitleWithAccent><p>Hello, <span>my name is</span></p></SubtitleWithAccent>
-        {/* Title */}
-        <Title><span>{name.split(' ')[0]}</span> {name.split(' ')[1]}</Title>
-        {/* Subtitle Bottom */}
-        <SubtitleWithHandwriting>I am a <span>{ occupation }</span></SubtitleWithHandwriting>
-      </div>
+      <Titles>
+        <SubtiteTop><p>Hello, <span>my name is</span></p></SubtiteTop>
+        <TitleContainer><span>{name.split(' ')[0]}</span> {name.split(' ')[1]}</TitleContainer>
+        <SubtiteBottom>I am a <span>{occupation}</span></SubtiteBottom>
+      </Titles>
 
       {/* Description */}
-      <div>
-        <div>
-          <p>{intro}</p>
-        </div>
-      </div>
+      <DescriptionContainer><p>{intro}</p></DescriptionContainer>
 
       {/* Socials */}
       <Socials />
 
       {/* Download Button */}
       <div></div>
-    </div>
+    </Container>
   );
 }
 
-const Description = styled.div``;
+const Container = styled.div`
+  padding-top: 40px;
+  position: relative;
+
+  @media screen and (min-width: 1700px) {
+    padding-top: 20px;
+  }
+`;
+
+const Titles = styled.div`
+  max-width: 640px;
+  /* z-index: 3; */
+
+  @media screen and (max-width: 1024px) {
+    margin-bottom: 760px;
+    max-width: 100%;
+  }
+
+  @media screen and (max-width: 767px) {
+    margin-bottom: 420px;
+    min-height: 160px;
+  }
+`;
+
+const SubtiteTop = styled(SubtitleWithAccent)``;
+
+const SubtiteBottom = styled(SubtitleWithHandwriting)``;
+
+const TitleContainer = styled(Title)``;
+
+const DescriptionContainer = styled(Description)`
+  padding: 40px 0 40px 0;
+  max-width: 520px;
+`;
 
 const Download = styled.div``;
