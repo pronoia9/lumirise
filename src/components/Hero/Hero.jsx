dcb74fa2dce494258fc468f6aac2360003435c5c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-16 19:39:08 +0300

    move background text component to hoc

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,26 +1,18 @@
 import { styled } from 'styled-components';
 
-import { HeroBackgroundText, HeroIntro, HeroImage } from './';
+import { HeroIntro, HeroImage } from './';
 import { SectionWrapper } from '../../hoc';
-import { heroData } from '../../utils/data';
 
 const Hero = () => {
   return (
-    <Wrapper className='lui-started v-line v-line-left'>
-      <Content className='section hero-started'>
-        <HeroIntro />
-        <HeroImage />
-      </Content>
-      <HeroBackgroundText text={heroData.backgroundTitle} />
-    </Wrapper>
+    <Content className='section hero-started'>
+      <HeroIntro />
+      <HeroImage />
+    </Content>
   );
 };
 
 export default SectionWrapper(Hero, 'hero');
-
-const Wrapper = styled.div`
-  width: 100%;
-`;
 
 const Content = styled.div`
   position: relative;
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,6 +1,5 @@
 import Hero from './Hero';
-import HeroBackgroundText from './HeroBackgroundText';
 import HeroImage from './HeroImage';
 import HeroIntro from './HeroIntro';
 
-export { Hero, HeroBackgroundText, HeroImage, HeroIntro };
+export { Hero, HeroImage, HeroIntro };
