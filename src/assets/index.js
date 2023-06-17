6ec0a25b6e03a1c2a740d71663de6c5a67043f4a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-17 20:23:21 +0300

    export blender project images

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,5 +10,28 @@ import punymath from './projects/puny-math.jpg';
 import stellarcountdown from './projects/stellar-countdown.jpg';
 import teetailor from './projects/tee-tailor.jpg';
 import westsum from './projects/westsum.jpg';
+// Blender
+import donut from './blender/donutguru-donut.jpg';
+import donutVid from './blender/donutguru-donut.mp4';
+import road from './blender/road.jpg';
+import ancientRuins from './blender/ancient-ruins.jpg';
+import cloudStation from './blender/cloud-station.jpg';
 
-export { logo, hi, bananarama, dallelyse, donutdelights, niftynoggin, organizedbliss, punymath, stellarcountdown, teetailor, westsum };
+export {
+  logo,
+  hi,
+  bananarama,
+  dallelyse,
+  donutdelights,
+  niftynoggin,
+  organizedbliss,
+  punymath,
+  stellarcountdown,
+  teetailor,
+  westsum,
+  donut,
+  donutVid,
+  road,
+  ancientRuins,
+  cloudStation,
+};
