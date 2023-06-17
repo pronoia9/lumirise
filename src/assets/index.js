394634e7743e7cc43d97a447f3c0df98ff126795
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-17 18:46:44 +0300

    add project images imports

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,14 @@
 import logo from './bitmojis/riding mr.unicorn.png';
 import hi from './bitmojis/hi.png';
+// Projects
+import bananarama from './projects/bananarama.jpg';
+import dallelyse from './projects/dall-elyse.jpg';
+import donutdelights from './projects/donut-delights.jpg';
+import niftynoggin from './projects/nifty-noggin.jpg';
+import organizedbliss from './projects/organized-bliss.jpg';
+import punymath from './projects/puny-math.jpg';
+import stellarcountdown from './projects/stellar-countdown.jpg';
+import teetailor from './projects/tee-tailor.jpg';
+import westsum from './projects/westsum.jpg';
 
-export { logo, hi };
+export { logo, hi, bananarama, dallelyse, donutdelights, niftynoggin, organizedbliss, punymath, stellarcountdown, teetailor, westsum };
