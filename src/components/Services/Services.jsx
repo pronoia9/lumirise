21e1c703773d2fc446202fe0396ed2241179301c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-17 13:20:32 +0300

    finish init services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,50 +1,38 @@
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Pagination, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { styled } from 'styled-components';
-import { motion } from 'framer-motion';
 import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-// import 'swiper/css/scrollbar';
-// import '../../../resources/Luique/style.css';
+import 'swiper/css/pagination';
 
+import { RightArrowSVG } from '..';
 import { SectionWrapper } from '../../hoc';
 import { servicesData } from '../../utils/data';
 import { Description } from '../../styles/TextStyles';
 
 const ServicesCard = ({ title, category, description }) => (
   <ServiceCardContainer className='services-item'>
-    <Description className='lui-subtitle'>
+    <ServiceCardSubtitle className='lui-subtitle'>
       <span>{category}</span>
-    </Description>
-    <div className='icon' />
-    <h5 className='lui-title'>
+    </ServiceCardSubtitle>
+    <ServiceCardIcon className='icon' />
+    <ServiceCardTitle className='lui-title'>
       <span>{title}</span>
-    </h5>
-    <div className='lui-text'>{description}</div>
-    <a href='#pricing-section' className='lnk'>
-      See Pricing
-    </a>
-    <div className='image' />
+    </ServiceCardTitle>
+    <ServiceCardDesction className='lui-text'>{description}</ServiceCardDesction>
+    <ServiceCardLink href='#works' className='lnk'>
+      See Works <RightArrowSVG />
+    </ServiceCardLink>
+    <ServiceCardPattern className='image' />
   </ServiceCardContainer>
 );
 
 const Services = () => {
   return (
     <Container>
-      <Swiper
-        modules={[Pagination, A11y]}
-        spaceBetween={50}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-      >
+      <Swiper modules={[Pagination, A11y]} spaceBetween={50} slidesPerView={3} pagination={{ clickable: true }} scrollbar={{ draggable: true }}>
         {servicesData.map((service, index) => (
-          <SwiperSlide>
-            <ServicesCard key={`service-${index}`} {...service} />
+          <SwiperSlide key={`service-${index}`}>
+            <ServicesCard {...service} />
           </SwiperSlide>
         ))}
       </Swiper>
@@ -55,93 +43,89 @@ const Services = () => {
 export default SectionWrapper(Services, 'services');
 
 const Container = styled.div`
-.swiper-slide {
-  height: auto;
-
-  & > div {
-    height: 100%;
-  }
-}
-  .js-services {
-    text-align: center;
-  }
-
-  .swiper-container.js-services {
+  .swiper-container {
     overflow: visible;
   }
 
-  .js-services .swiper-wrapper {
+  .swiper-wrapper {
     margin-bottom: 50px;
     text-align: left;
   }
 
-  .elementor-editor-active .js-services .swiper-wrapper .swiper-slide .services-item {
-    opacity: 1;
-  }
+  .swiper-slide {
+    height: auto;
 
-  .js-services .swiper-wrapper .swiper-slide .services-item {
-    transition: all 0.3s cubic-bezier(0.3, 0, 0.3, 1);
-    opacity: 0;
-  }
-
-  .js-services .swiper-wrapper .swiper-slide-visible .services-item {
-    opacity: 1;
-  }
-
-  .services-items {
-    margin-top: -40px;
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-  }
-
-  .services-col {
-    max-width: 270px;
-    flex: 0 0 50%;
-  }
-
-  .services-item {
-    position: relative;
-    overflow: hidden;
-    padding: 30px 35px;
-    background: var(--c-background3);
-    border-radius: 18px;
-  }
-
-  .services-item:first-child {
-    margin-top: 0;
-  }
-
-  .services-item .icon {
-    margin-top: 15px;
-    font-size: 41px;
-    min-height: 50px;
-    line-height: 1;
-    color: var(--c-accent);
-  }
-
-  .services-item .lui-text {
-    min-height: 175px;
-  }
-
-  .services-item .image,
-  .works-item .bg-img,
-  .testimonials-item .bg-img,
-  .pricing-item .bg-img {
-    position: absolute;
-    right: -24px;
-    bottom: -24px;
-    width: 141px;
-    height: 141px;
-    background-position: center center;
-    background-repeat: no-repeat;
-    background-size: contain;
-  }
-
-  .works-box.works-list .works-item .bg-img {
-    right: 43%;
-    bottom: 0;
+    & > div {
+      height: 100%;
+    }
   }
 `;
 
-const ServiceCardContainer = styled.div``;
+const ServiceCardContainer = styled.div`
+  position: relative;
+  overflow: hidden;
+  padding: 30px 35px;
+  background: var(--c-background3);
+  border-radius: 18px;
+  transition: all 0.3s cubic-bezier(0.3, 0, 0.3, 1);
+
+  &:first-child {
+    margin-top: 0;
+  }
+`;
+
+const ServiceCardIcon = styled.div`
+  margin-top: 15px;
+  font-size: 41px;
+  min-height: 50px;
+  line-height: 1;
+  color: var(--c-accent);
+`;
+
+const ServiceCardSubtitle = styled(Description)``;
+
+const ServiceCardTitle = styled.div``;
+
+const ServiceCardDesction = styled.div`
+  min-height: 175px;
+`;
+
+const ServiceCardLink = styled.a`
+  position: relative;
+  height: 1.375rem;
+  display: inline-block;
+  vertical-align: top;
+  color: var(--c-font);
+  font-size: 1rem;
+  font-weight: 700;
+  line-height: 1.375rem;
+  text-decoration: none;
+  transition: all 0.7s cubic-bezier(0.3, 0, 0.3, 1);
+
+  svg {
+    position: relative;
+    left: 0;
+    top: 1px;
+    width: 1rem;
+    height: auto;
+    margin-left: 1.25rem;
+    font-weight: 900;
+    fill: var(--c-accent);
+    transition: all 0.3s cubic-bezier(0.3, 0, 0.3, 1);
+  }
+
+  &:hover svg {
+    margin-left: 0.9rem;
+  }
+`;
+
+const ServiceCardPattern = styled.div`
+  position: absolute;
+  right: -24px;
+  bottom: -24px;
+  width: 141px;
+  height: 141px;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: contain;
+`;
