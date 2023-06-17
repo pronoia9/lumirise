439c1d5784a683e0444b3c67ec3b1cae39338064
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-17 21:53:22 +0300

    init works component with the new card

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,10 +1,24 @@
 import { styled } from 'styled-components';
-import { motion } from 'framer-motion';
 
+import { Card } from '..';
 import { SectionWrapper } from '../../hoc';
+import { projectsData } from '../../utils/data';
 
 const Works = () => {
-  return <div>Works</div>;
+  return (
+    <Container>
+      {projectsData.projects.map((project, index) => (
+        <Card key={`project-${index}`} {...project} descheight={275} section='Works'>
+          {/* IMAGE HERE */}
+        </Card>
+      ))}
+    </Container>
+  );
 };
-
 export default SectionWrapper(Works, 'works');
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 2rem;
+`;
