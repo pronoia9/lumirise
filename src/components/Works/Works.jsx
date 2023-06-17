9e732b44283c13c7cbe954d57088023db2d4228c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-17 22:57:06 +0300

    add init button

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -3,24 +3,41 @@ import { styled } from 'styled-components';
 import { Card } from '..';
 import { SectionWrapper } from '../../hoc';
 import { projectsData } from '../../utils/data';
+import { UnfilledButton } from '../../styles/ButtonStyles';
 
 const Works = () => {
   return (
     <Container>
-      {projectsData.projects.map((project, index) => (
-        <Card key={`project-${index}`} {...project} descheight={275} section='Works'>
-          {/* IMAGE HERE */}
-          <Image className='image'>
-            <img src={project.image} />
-          </Image>
-        </Card>
-      ))}
+      <Wrapper>
+        {projectsData.projects.slice(0, 6).map((project, index) => (
+          <Card key={`project-${index}`} {...project} descheight={275} section='Works'>
+            {/* IMAGE HERE */}
+            <Image className='image'>
+              <img src={project.image} />
+            </Image>
+          </Card>
+        ))}
+      </Wrapper>
+
+      {projectsData.projects.length > 6 && (
+        <UnfilledButton className='bts'>
+          <p>Load More</p>
+        </UnfilledButton>
+      )}
     </Container>
   );
 };
 export default SectionWrapper(Works, 'works');
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+`;
+
+const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   gap: 2rem;
