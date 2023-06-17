c5afe9b38a2d3464e375d917b639299c3d49fb33
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-17 22:30:13 +0300

    add project images

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,5 +1,6 @@
 import { styled } from 'styled-components';
 
+import { logo } from '../../assets';
 import { Card } from '..';
 import { SectionWrapper } from '../../hoc';
 import { projectsData } from '../../utils/data';
@@ -10,6 +11,9 @@ const Works = () => {
       {projectsData.projects.map((project, index) => (
         <Card key={`project-${index}`} {...project} descheight={275} section='Works'>
           {/* IMAGE HERE */}
+          <Image className='image'>
+            <img src={project.image} />
+          </Image>
         </Card>
       ))}
     </Container>
@@ -21,4 +25,27 @@ const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   gap: 2rem;
+`;
+
+const Image = styled.div`
+  width: 100%;
+  height: 250px;
+  border-radius: 18px;
+  overflow: hidden;
+  margin-bottom: 30px;
+
+  img {
+    width: 100%;
+    height: 250px;
+    border-radius: 18px;
+    object-fit: cover;
+    transform: scale(1);
+    transition: transform 0.6s cubic-bezier(0.3, 0, 0.3, 1);
+  }
+
+  &:hover {
+    img {
+      transform: scale(1.1);
+    }
+  }
 `;
