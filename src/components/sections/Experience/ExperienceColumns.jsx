60ceccf3719141b57ad0767f6155358367d1ec39
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-19 19:13:27 +0300

    fix padding

--- src/components/sections/Experience/ExperienceColumns.jsx
+++ src/components/sections/Experience/ExperienceColumns.jsx
@@ -22,7 +22,7 @@ export default function ExperienceColumns({ column, data }) {
 const Container = styled.div`
   h5 {
     margin: 0;
-    padding: 0 0 ${rem(30)} 0;
+    padding: 0 0 ${rem(24)} 0;
     line-height: 1;
     text-align: center;
     border-bottom: ${rem(2)} solid var(--c-lineBorder);
