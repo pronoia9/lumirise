8b66a36d46c9f9d6d655c5a9a69c862f72e8fa33
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-20 00:27:10 +0300

    finish footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,45 @@
 import { styled } from 'styled-components';
+import Socials from '../components/Socials';
+import { rem } from '../../utils/utils';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Container>
+      <Socials />
+
+      <div>
+        © 2023 <span>@pronoia9</span>. All rights reserved
+      </div>
+
+      <div>
+        Originally by <span>bslthemes</span>
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.footer`
+  padding: ${rem(60)} ${rem(12)};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-top: 1px solid var(--c-lineShadow);
+  font-size: 13px;
+  font-weight: 700;
+  letter-spacing: 0.05em;
+  text-transform: uppercase;
+  line-height: 37px;
+
+  span {
+    color: var(--c-accent);
+  }
+
+  svg {
+    max-width: ${rem(22)};
+    max-height: ${rem(22)};
+  }
+
+  @media (max-width: ${rem(767)}) {
+    flex-direction: column;
+  }
+`;
