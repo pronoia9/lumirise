c0e6d514ad87ff5a3f62ccfa8c86e0b4d4f6cf41
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-21 23:57:30 +0300

    add set frame + cursor to works card image

--- src/components/sections/Works/WorksCard.jsx
+++ src/components/sections/Works/WorksCard.jsx
@@ -1,12 +1,15 @@
 import { styled } from 'styled-components';
 
 import { Card } from '../..';
+import { dataStore } from '../../../store/dataStore';
 import { cardMotion, rem } from '../../../utils';
 
 export default function WorksCard({ filterKey, project, index }) {
+  const setFrameLink = dataStore((state) => state.setFrameLink);
+
   return (
     <Card key={`projects-${filterKey}-${index}`} {...project} section='Works' motion={cardMotion}>
-      <Image>
+      <Image onClick={() => setFrameLink(project?.links?.live)} link={`${project?.links?.live.length > 0}`}>
         <img src={project.image} />
       </Image>
     </Card>
@@ -15,14 +18,15 @@ export default function WorksCard({ filterKey, project, index }) {
 
 const Image = styled.div`
   width: 100%;
-  height: ${rem(180)};
+  height: ${rem(250)};
   border-radius: ${rem(18)};
   overflow: hidden;
   margin-bottom: ${rem(30)};
+  cursor: ${({ link }) => link === 'true' && 'zoom-in'};
 
   img {
     width: 100%;
-    height: ${rem(180)};
+    height: ${rem(250)};
     border-radius: ${rem(18)};
     object-fit: cover;
     transform: scale(1);
