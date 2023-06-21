efc74ac9beb30b217aee13bb3e3f52abf8773ef1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-21 18:00:16 +0300

    map projects with works list item

--- src/components/sections/Works/WorksList.jsx
+++ src/components/sections/Works/WorksList.jsx
@@ -1,3 +1,11 @@
-export default function WorksList() {
-  return <div>WorksList</div>;
+import WorksListItem from "./WorksListItem";
+
+export default function WorksList({ projects, count, filterKey }) {
+  return (
+    <div>
+      {projects.slice(0, count).map((project, index) => (
+        <WorksListItem key={`works-list-${filterKey}-${index}`} filterKey={filterKey} project={project} index={index} />
+      ))}
+    </div>
+  );
 }
